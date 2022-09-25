34e944648865bf59af43222fd6852f9a26c56587
sunnatbee <sunnatbekyusupov@icloud.com>
2022-09-26 02:49:08 +0500

    Initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -14,26 +14,31 @@ $(".responsive").slick({
   slidesToScroll: 3,
   responsive: [
     {
-      breakpoint: 1200,
+      breakpoint: 1024,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2,
-        infinite: true,
-        dots: true,
+        slidesToScroll: 1,
       },
     },
     {
-      breakpoint: 900,
+      breakpoint: 992,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToShow: 2.5,
+        slidesToScroll: 1,
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1.5,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 500,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 4,
+        slidesToScroll: 1,
       },
     },
   ],
@@ -44,10 +49,10 @@ $(".responsive").slick({
 //   slidesToShow: 3,
 //   slidesToScroll: 3,
 // });
-$(".autoplay").slick({
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  autoplay: true,
-  autoplaySpeed: 2000,
+// $(".autoplay").slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 3,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
   
-});
+// });
